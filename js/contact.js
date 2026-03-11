83502a330465adb37da13b0422a8b3d5a00c07e7
Namrata Shekhar <shekharnamrata15@gmail.com>
2026-03-11 21:33:37 +0530

    changes done

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,30 @@
 alert("Welcome to blush & Bloom Contact Page🌸.");
 
-let visitorCount = 0;
-visitorCount++;
+let visitorCount = localStorage.getItem("visitorCount");
+
+if (visitorCount === null) {
+    visitorCount = 1;
+} else {
+    visitorCount = Number(visitorCount) + 1;
+}
+
+localStorage.setItem("visitorCount", visitorCount);
 
 function sendMessage() {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
 
+    localStorage.setItem("userName", name);
+    localStorage.setItem("userEmail", email);
+    localStorage.setItem("Message", message);
 
     alert(`Hello ! ${name}.
     Thank you for contacting Blush & Bloom we received your message : ${message}
     Our team will contact you soon on ${email}`);
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
 
 }
