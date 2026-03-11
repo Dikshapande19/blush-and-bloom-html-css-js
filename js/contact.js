5a5c3c86ce4696ba354d4fe3baacf9cb2766fe4f
Namrata Shekhar <shekharnamrata15@gmail.com>
2026-03-11 19:58:10 +0530

    changes done

--- js/contact.js
+++ js/contact.js
@@ -8,13 +8,9 @@ function sendMessage() {
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
 
-    
 
     alert(`Hello ! ${name}.
     Thank you for contacting Blush & Bloom we received your message : ${message}
-    
-     
     Our team will contact you soon on ${email}`);
-
 
 }
